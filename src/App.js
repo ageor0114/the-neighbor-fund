80f4c36e0fe183b1f13730bb57ab80560361e42d
Austin George <ageorge8000@gmail.com>
2020-12-22 15:05:25 -0500

    init neighbor fund home

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>The Neighbor Fund</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Discover organizations in your neighborhood that might be willing to support the cause.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
