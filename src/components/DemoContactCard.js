ba3d8758a0fe461af9c2746bf4c8f7a2d2cbca30
Austin George <ageorge8000@gmail.com>
2020-12-25 03:16:35 -0500

    Reformatted Contact Cards & Added Opacity Gradient

--- src/components/DemoContactCard.js
+++ src/components/DemoContactCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-function DemoContactCard({title,text}) {
+function DemoContactCard({title,text,key}) {
 
   return (
-    <div className="demo-contact-card">
+    <div className="demo-contact-card" id={(title[0] === "C" && "faded") || (title[0] === "N" && "top")}>
        <h1>{title}</h1>
        <p>{text}</p>
+       <p>{key}</p>
        <div className="demo-contact">Contact</div>
     </div>
   );
